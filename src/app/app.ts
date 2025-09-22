7a9f86c6c8b99bfa5b2276761609d3fa463626d2
efrainbautista <efrain@thibau.io>
2025-09-22 08:22:18 -0500

    feat(layout): add layout component with structure and navigation.

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,15 @@ import {TranslateService} from '@ngx-translate/core';
 import {Home} from './shared/presentation/views/home/home';
 import {About} from './shared/presentation/views/about/about';
 import {PageNotFound} from './shared/presentation/views/page-not-found/page-not-found';
+import {FooterContent} from './shared/presentation/components/footer-content/footer-content';
 
 @Component({
   selector: 'app-root',
   imports: [
     Home,
     About,
-    PageNotFound
+    PageNotFound,
+    FooterContent
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
