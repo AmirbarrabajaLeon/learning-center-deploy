dff5e887000882c9c94f740c4b627c90183542bf
efrainbautista <efrain@thibau.io>
2025-09-29 08:14:13 -0500

    feat(env): update environment configuration files for production and development.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  // In real lifecycles, this file should not be part of version control
   production: true,
-  // Server Base URL for REST API
-  serverBaseUrl: 'http://localhost:3000/api/v1',
-  coursesEndpointPath: '/courses',
+  platformProviderApiBaseUrl: 'http://localhost:3000/api/v1',
+  platformProviderCategoriesEndpointPath: '/categories',
+  platformProviderCoursesEndpointPath: '/courses',
+  logoProviderApiBaseUrl: 'https://logo.clearbit.com/'
 };
