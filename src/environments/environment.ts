8d1e36e7b6d1aa6c3e10f8c2a93234b61810459c
efrainbautista <efrain@thibau.io>
2025-10-03 08:26:03 -0500

    feat(environments): update api base url in production.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  platformProviderApiBaseUrl: 'http://localhost:3000/api/v1',
+  platformProviderApiBaseUrl: 'https://mockapi.azurewebsites.net/api/v1',
   platformProviderCategoriesEndpointPath: '/categories',
   platformProviderCoursesEndpointPath: '/courses',
   logoProviderApiBaseUrl: 'https://logo.clearbit.com/'
