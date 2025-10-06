34057bcf8d456dbb504f86d6eca6afc729daa78c
AmirbarrabajaLeon <amircasan@gmail.com>
2025-10-06 08:51:23 -0500

    chore: update platformProviderApiBaseUrl to use production endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  platformProviderApiBaseUrl: 'http://localhost:3000/api/v1',
+  platformProviderApiBaseUrl: 'https://jsonservertest1.azurewebsites.net',
   platformProviderCategoriesEndpointPath: '/categories',
   platformProviderCoursesEndpointPath: '/courses',
   logoProviderApiBaseUrl: 'https://logo.clearbit.com/'
